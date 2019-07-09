178604852f2e87d19826f3f420a96d55e86b156b
0LL1 <o.a.h.rautiainen@gmail.com>
2019-07-10 01:36:10 +0300

    4.10

--- tests/blog-api.test.js
+++ tests/blog-api.test.js
@@ -71,6 +71,29 @@ test('there is an id field', async () => {
   expect(response.body[0].id).toBeDefined()
 })
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Test title',
+    author: 'Olli',
+    url: 'www.test.com',
+    likes: 100
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(r => r.title)
+
+  expect(response.body.length).toBe(initialBlogs.length + 1)
+
+  expect(titles).toContain('Test title')
+})
+
 beforeEach(async () => {
   await Blog.deleteMany({})
 
